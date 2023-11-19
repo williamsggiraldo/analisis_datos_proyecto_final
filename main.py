ca2cba56dad8f843085bb210df7a1ab724f59d05
Williams Gutierrez <ggiraldow@gmail.com>
2023-11-18 22:57:17 -0500

    delete error handle

--- main.py
+++ main.py
@@ -10,11 +10,8 @@ modelo_mlp = joblib.load('resources/modelo_mlp.pkl')
 @app.post("/predict")
 def predict(request: dict):
     print("request_body = ", request)
-    if request is None:
-        raise HTTPException(status_code=400, detail='Request body cannot be null')
-    else:
-        values_list = list(request.values())
-        data = np.array(values_list, dtype=int)
-        print("values", data)
-        prediction = modelo_mlp.predict([data])
-        return prediction.tolist()
+    values_list = list(request.values())
+    data = np.array(values_list, dtype=int)
+    print("values", data)
+    prediction = modelo_mlp.predict([data])
+    return prediction.tolist()
