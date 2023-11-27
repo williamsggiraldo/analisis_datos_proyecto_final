d6cd563a273dc5f518dbc28b34cea9c36b9e7883
Williams Gutierrez <ggiraldow@gmail.com>
2023-11-27 18:55:40 -0500

    add configuration for cors origin

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import numpy as np
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 modelo_mlp = joblib.load('resources/modelo_mlp.pkl')
 
